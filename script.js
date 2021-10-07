62d211403a6b82d09e783b63538408238ca9920d
lrnzcas <lrnzo.castellano@gmail.com>
2021-10-07 10:19:08 +0800

    2 files modified

--- script.js
+++ script.js
@@ -1,3 +1,69 @@
+window.addEventListener('scroll', scrollFunc, true);
+
 resume = document.getElementById('resume');
 
-console.log(resume.offsetTop);
+function scrollFunc(e) {
+  if (window.scrollY > 1100) {
+    document.head.appendChild(styleSheet);
+  }
+}
+
+var styles = `
+    @keyframes html5 {
+  from {
+    width: 0;
+    background-color: #fe6786;
+    border-radius: 0;
+  }
+  to {
+    width: 75%;
+    background-color: #9acd32;
+    border-radius: 10px;
+  }
+}
+
+@keyframes js {
+  from {
+    width: 0;
+    background-color: #fe6786;
+    border-radius: 0;
+  }
+  to {
+    width: 80%;
+    background-color: #5e7f1c;
+    border-radius: 10px;
+  }
+}
+
+@keyframes css {
+  from {
+    width: 0;
+    background-color: #fe6786;
+    border-radius: 0;
+  }
+
+  to {
+    width: 60%;
+    background-color: #bfff3e;
+    border-radius: 10px;
+  }
+}
+
+@keyframes python {
+  from {
+    width: 0;
+    background-color: #fe6786;
+    border-radius: 0;
+  }
+
+  to {
+    width: 30%;
+    background-color: #ffd200;
+    border-radius: 10px;
+  }
+}
+`;
+
+var styleSheet = document.createElement('style');
+styleSheet.type = 'text/css';
+styleSheet.innerText = styles;
